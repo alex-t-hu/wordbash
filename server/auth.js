d6010797cea4fec619572b5cb5c4afa31ad23d24
Rowechen Zhong <rowechen@gmail.com>
2023-01-15 18:13:02 -0500

    Auth Client ID

--- server/auth.js
+++ server/auth.js
@@ -4,7 +4,7 @@ const socketManager = require("./server-socket");
 
 // create a new OAuth client used to verify google sign-in
 //    TODO: replace with your own CLIENT_ID
-const CLIENT_ID = "FILL IN CLIENT ID";
+const CLIENT_ID = "414404150327-qhpp3e5ihem4nvr38ba1vifiv04633ff.apps.googleusercontent.com";
 const client = new OAuth2Client(CLIENT_ID);
 
 // accepts a login token from the frontend, and verifies that it's legit
