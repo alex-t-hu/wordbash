97a33c906062304191da5f31b258a377ba50dcbe
Rowechen Zhong <rowechen@gmail.com>
2023-01-15 18:23:00 -0500

    setup

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ const socketManager = require("./server-socket");
 
 // Server configuration below
 // TODO change connection URL after setting up your team database
-const mongoConnectionURL = "FILL ME IN";
+const mongoConnectionURL = "mongodb+srv://rowechen:nArd2o4SQ9qhfbKv@weblab2023.bla2rem.mongodb.net/?retryWrites=true&w=majority";
 // TODO change database name to the name you chose
 const databaseName = "FILL ME IN";
 
