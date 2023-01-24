4d45f8ac4843dcd75e5691cb5f13dedc4139b1d8
Alexander Hu <superalexanderhu@gmail.com>
2023-01-23 20:34:06 -0500

    prompt2

--- client/src/components/pages/Prompt.js
+++ client/src/components/pages/Prompt.js
@@ -43,20 +43,94 @@ const Prompt = (props) => {
         get("/api/getGame", { gameID: props.gameID }).then((game) => {
             setUserGameId(game["players"][props.userId]["id"]);
             firstPrompt = game["prompts"][userGameId]["prompt_0"];
-            secondPrompt = game["prompts"][(userGameId +1)% game["num_players"]]["prompt_1"];
+            secondPrompt = game["prompts"][(userGameId -1+game["num_players"])% game["num_players"]]["prompt_1"];
         });
     }, []);
 
 if (hasAnsweredFirst) {
     return (
-        
-
-    )
+        <div className="Prompt-container">
+            <div className="Prompt-prompt">
+                <h1>{secondPrompt}</h1>
+            </div>
+            <div className="Prompt-response">
+                <textarea
+                    className="Prompt-textarea"
+                    type="text"
+                    placeholder="Enter your response here"
+                    value={secondResponse}
+                    onChange={(event) => {
+                        setSecondResponse(event.target.value);
+                    }}
+                />
+            </div>
+            <div className="Prompt-button">
+                <button
+                    className="Prompt-button"
+                    type="submit"
+                    value="Submit"
+                    onClick={() => {
+                        post("/api/submitResponse", {
+                            gameID: props.gameID,
+                            playerID: userGameId,
+                            response: secondResponse,
+                        });
+                        props.setGame({
+                            ...props.game,
+                            responses: {
+                                ...props.game.responses,
+                                [userGameId]: secondResponse,
+                            },
+                        });
+                    }}
+                >
+                    Submit
+                </button>
+            </div>
+        </div>
+    );
 } else{
     return (
-
-    )
-
+        <div className="Prompt-container">
+            <div className="Prompt-prompt">
+                <h1>{firstPrompt}</h1>
+            </div>
+            <div className="Prompt-response">
+                <textarea
+                    className="Prompt-textarea"
+                    type="text"
+                    placeholder="Enter your response here"
+                    value={firstResponse}
+                    onChange={(event) => {
+                        setFirstResponse(event.target.value);
+                    }}
+                />
+            </div>
+            <div className="Prompt-button">
+                <button
+                    className="Prompt-button"
+                    type="submit"
+                    value="Submit"
+                    onClick={() => {
+                        post("/api/submitResponse", {
+                            gameID: props.gameID,
+                            playerID: userGameId,
+                            response: firstResponse,
+                        });
+                        props.setGame({
+                            ...props.game,
+                            responses: {
+                                ...props.game.responses,
+                                [userGameId]: firstResponse,
+                            },
+                        });
+                    }}
+                >
+                    Submit
+                </button>
+            </div>
+        </div>
+    );
 }
   return (
     <>
@@ -93,4 +167,4 @@ if (hasAnsweredFirst) {
 //   </>
 // );
 
-export default App;
+export default Prompt;
