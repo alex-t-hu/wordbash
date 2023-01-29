5497e7793c526540b56f1daed3ee528f0cb3885d
Alexander Hu <superalexanderhu@gmail.com>
2023-01-28 23:51:22 -0500

    add round that doesnt do anything

--- client/src/components/pages/Ingame.js
+++ client/src/components/pages/Ingame.js
@@ -4,7 +4,7 @@ import { Router } from "@reach/router";
 import Prompt from "./Prompt.js";
 import Voting from "./Voting.js";
 import SidebarUserList from "../modules/SidebarUserList/SidebarUserList.js";
-
+import Round from "./Round.js";
 
 import "../../utilities.css";
 
@@ -15,13 +15,15 @@ import "../../utilities.css";
 const Ingame = (props) => {
   // console.log("GAMEEE",props.userId);
   // console.log(props.gameID);
+  const [currentRound, setCurrentRound] = useState(0);
   return (
     <>      
       <div className="h-screen bg-gray-50">
         <SidebarUserList users={props.game["players"]}/>
         <Router>
-            <Prompt path=":gameID/prompt" userId={props.userId} gameID ={props.gameID} game = {props.game} setGame = {props.setGame}/>
-            <Voting path=":gameID/voting" userId={props.userId} gameID ={props.gameID} game = {props.game} setGame = {props.setGame}/>
+            <Prompt path=":gameID/prompt" userId={props.userId} gameID ={props.gameID} game = {props.game} setGame = {props.setGame} currentRound = {currentRound} setCurrentRound = {setCurrentRound}/>
+            <Voting path=":gameID/voting" userId={props.userId} gameID ={props.gameID} game = {props.game} setGame = {props.setGame} currentRound = {currentRound} setCurrentRound = {setCurrentRound}/>
+            <Round path=":gameID/round" userId={props.userId} gameID ={props.gameID} game = {props.game} setGame = {props.setGame} currentRound = {currentRound} setCurrentRound = {setCurrentRound}/>
         </Router>
       </div>
     </>
