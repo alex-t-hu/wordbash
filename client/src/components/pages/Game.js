382d10bfe367f1b325d83da7ad4a1aa5d203925f
Andy Jiang <andyjiang2016@gmail.com>
2023-01-29 19:59:38 -0500

    chatbox

--- client/src/components/pages/Game.js
+++ client/src/components/pages/Game.js
@@ -12,6 +12,8 @@ import Background from "../modules/Background.js"
 
 import Collapsible from 'react-collapsible';
 
+import { Resizable, Draggable } from 'react-resizable';
+
 import "../../utilities.css";
 
 
@@ -21,6 +23,13 @@ import "../../utilities.css";
 const Game = (props) => {
   // console.log("GAMEEE",props.userId);
   // console.log(props.gameID);
+
+  const [dims, setDims] = useState([200, 200]);
+
+  // On top layout
+  const onResize = (event, {element, size, handle}) => {
+    setDims([size.width, size.height]);
+  };
 
   const [chatOpen, setChatOpen] = useState(true);
   return (
@@ -35,28 +44,23 @@ const Game = (props) => {
               <Ingame path="/*" userId={props.userId} gameID ={props.gameID} game = {props.game} setGame = {props.setGame}/>
             </Router>
           </div>
-
-          <div className="flex flex-col justify-end">
+          <div className="flex flex-col justify-center">
             <button
-              className="bg-white w-[70px] hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow flex text-l text-black items-center cursor-pointer bottom-6 z-50"
+              className={`${chatOpen ? "ease-in-out duration-500 translate-x-[50%]" : "ease-in-out duration-500 translate-x-0"} bg-white w-14 h-14 hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded-full shadow flex text-l text-black items-center cursor-pointer bottom-6 z-50`}
               onClick={() => setChatOpen(!chatOpen)}
             >
-              {chatOpen ? (
-                <h1>{"Chat <"}</h1>
-              ) : (
-                <h1>{"Chat >"}</h1>
-              )}
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
+              </svg>
             </button>
           </div>
           
-          <div className={`bg-[#EEEEEE] right-0 text-white z-20 ease-in-out duration-300 
+          <div className={`h-full right-0 text-white z-20 ease-in-out duration-500 
           ${
-            chatOpen ? "w-0" : "w-[30%]"
+            chatOpen ? "w-[30%]" : "w-0"
           }`}>
             <Chatbook userId={props.userId} gameID={props.gameID} game={props.game} setGame={props.setGame}/>
           </div>
-          
-
         </div>
         <Background />
       </div>
