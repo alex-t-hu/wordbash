95e4815960367e75460893431925b29484a18069
Alexander Hu <superalexanderhu@gmail.com>
2023-02-01 04:46:07 -0500

    resultspage

--- client/src/components/pages/FinalResults.js
+++ client/src/components/pages/FinalResults.js
@@ -42,15 +42,16 @@ const FinalResults = (props) => {
             width={window.innerWidth}
             height={window.innerHeight}
         />
-        {/* <div className="finalResults-container"> */}
+        <div className="finalResults-container">
             <div className="w-[100%] h-[100%]">
                 <FinalUserList className="" users={props.game["players"]}/>
             </div>
             {/* {props.game && props.game["players"] ? <SortedPlayerList players={props.game["players"]}/> : <div>Loading...</div>} */}
-            <button className=" u-flex-alignCenter" onClick = {onSubmit}>
-                Return to Lobby
+            <button className="relative w-[90%] flex justify-center bg-white hover:bg-gray-100 text-3xl text-gray-800 font-semibold py-2 my-2 px-4 border border-gray-400 rounded shadow transition ease-in-out delay-50 hover:scale-[1.05] hover:scale-130 duration-300" onClick = {onSubmit}>
+                
+                <div>Return to Lobby</div>
             </button> 
-        {/* </div> */}
+        </div>
         </>
     );
     
