7b8179c7671b0b8c2f6f61942f5e769887d6170e
Rowechen Zhong <rowechen@gmail.com>
2023-01-31 21:46:46 -0500

    VR2

--- client/src/components/pages/Voting/VotingResults2.js
+++ client/src/components/pages/Voting/VotingResults2.js
@@ -42,14 +42,39 @@ const VotingResults2 = (props) => {
 
             <div className="flex flex-row w-full">
                 <div className="w-[50%] p-[5%] flex flex-row w-full align-items justify-between">
-                    <div className ="w-[30%] aspect-auto">
+                    <div className ="w-[30%] aspect-auto flex flex-col h-full align-items justify-around">
                         <img src = {prompt['response_0_avatar']} />
+                        <div className="bg-amber-300 rounded-2xl p-1 flex flex-row w-full align-items justify-around">
                         <h1 className="text-center text-4xl">{prompt['response_0_person_name']}</h1>
                         </div>
+                    </div>
                     <div className = "w-[60%]" >
                         <PromptResponse hoverable={false} message={prompt['response_0_answer']} author={prompt['response_0_person_name']} dir="left"/>
+                    </div>
+                    
+                </div>
+                <div className="w-[50%] p-[5%] flex flex-row w-full align-items justify-between">
+                    <div className = "w-[60%]" >
+                        <PromptResponse className = "w-[80%]" hoverable={false} message={prompt['response_1_answer']} author={prompt['response_1_person_name']} dir="right"/>
+                    </div>
+
+
+                    <div className ="w-[30%] aspect-auto flex flex-col h-full align-items justify-around">
+                        <img src = {prompt['response_1_avatar']} />
+                        <div className="bg-amber-300 rounded-2xl p-1 flex flex-row w-full align-items justify-around">
+                        <h1 className="text-center text-4xl">{prompt['response_1_person_name']}</h1>
+                        </div>
+
+                        </div>
+                </div>
+
+
+            </div>
+
+            <div className="flex flex-row w-full">
+                <div className="w-[50%] p-[5%] flex flex-row w-full align-items justify-between">
                         {prompt["response_0_vote_names"].map((player) => {    
-                            return (<div className="bg-gray-50 rounded-2xl p-1 flex flex-row w-full align-items justify-around">
+                            return (<div className="bg-amber-300 rounded-2xl p-1 flex flex-row w-full align-items justify-around">
                                         
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-[20%] aspect-auto">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z" />
@@ -68,15 +93,14 @@ const VotingResults2 = (props) => {
                     
                     </div>  );
                         })} 
-                    </div>
                     
                 </div>
+
+
                 <div className="w-[50%] p-[5%] flex flex-row w-full align-items justify-between">
-                    <div className = "w-[60%]" >
-                        <PromptResponse className = "w-[80%]" hoverable={false} message={prompt['response_1_answer']} author={prompt['response_1_person_name']} dir="right"/>
                         {prompt["response_1_vote_names"].map((player) => {
                             return (
-                                <div className="bg-gray-50 rounded-2xl p-1 flex flex-row w-full align-items justify-around">
+                                <div className="bg-amber-300 rounded-2xl p-1 flex flex-row w-full align-items justify-around">
                                         
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-[20%] aspect-auto">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 01-1.043 3.296 3.745 3.745 0 01-3.296 1.043A3.745 3.745 0 0112 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 01-3.296-1.043 3.745 3.745 0 01-1.043-3.296A3.745 3.745 0 013 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 011.043-3.296 3.746 3.746 0 013.296-1.043A3.746 3.746 0 0112 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 013.296 1.043 3.746 3.746 0 011.043 3.296A3.745 3.745 0 0121 12z" />
@@ -95,18 +119,14 @@ const VotingResults2 = (props) => {
                             
                                 </div> );
                         })} 
-                    </div>
 
-
-                    <div className ="w-[30%] aspect-auto">
-                        <img src = {prompt['response_1_avatar']} />
-                        <h1 className="text-center text-4xl">{prompt['response_1_person_name']}</h1>
-
-                        </div>
                 </div>
 
 
             </div>
+
+
+
             
             <div className="flex-grow"></div>
             <div className="w-full flex justify-center my-[40px]">
